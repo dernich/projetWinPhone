0f3370583740ae985e12ec8e34c6af70ed3714b9
dernich <dernivoix@hotmail.com>
2015-11-20 19:03:54 +0100

    menu navigation avec listView patient

---
+++ DrawerLayoutDemo/DrawerLayoutDemo/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace DrawerLayoutDemo
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            DrawerLayout.InitializeDrawerLayout();
+
+            string[] menuItems = new string[4] { "Feuille de route", "Supprimer Patient", "Attribution", "Deconnexion"};
+            ListMenuItems.ItemsSource = menuItems.ToList();
+
+            string[] patientItems = new string[5] { "Degraux", "Dernivoix", "Doumont", "Durant", "Lejeune"};
+            ListPatientItems.ItemsSource = patientItems.ToList();
+        }
+        private void DrawerIcon_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (DrawerLayout.IsDrawerOpen)
+                DrawerLayout.CloseDrawer();
+            else
+                DrawerLayout.OpenDrawer();
+        }
+
+        private void Navigate_InfoPage(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(InformationPage));
+        }
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+            Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+        void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+        {
+            if (DrawerLayout.IsDrawerOpen)
+            {
+                DrawerLayout.CloseDrawer();
+                e.Handled = true;
+            }
+            else
+            {
+                Application.Current.Exit();
+            }
+        }
+    }
+}
