dba17ba4919a8641b1803e74f2d1765cebcc3d4b
dernich <dernivoix@hotmail.com>
2015-12-03 21:39:45 +0100

    webAPI

---
+++ WebApiNurseAgenda/WebApiNurseAgenda/Controllers/patientsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiNurseAgenda.Models;
+
+namespace WebApiNurseAgenda.Controllers
+{
+    public class patientsController : ApiController
+    {
+        private nurseAgendaEntities db = new nurseAgendaEntities();
+
+        // GET: api/patients
+        public IQueryable<patient> Getpatients()
+        {
+            return db.patients;
+        }
+
+        // GET: api/patients/5
+        [ResponseType(typeof(patient))]
+        public IHttpActionResult Getpatient(decimal id)
+        {
+            patient patient = db.patients.Find(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(patient);
+        }
+
+        // PUT: api/patients/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putpatient(decimal id, patient patient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != patient.idPatient)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(patient).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!patientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/patients
+        [ResponseType(typeof(patient))]
+        public IHttpActionResult Postpatient(patient patient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.patients.Add(patient);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (patientExists(patient.idPatient))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = patient.idPatient }, patient);
+        }
+
+        // DELETE: api/patients/5
+        [ResponseType(typeof(patient))]
+        public IHttpActionResult Deletepatient(decimal id)
+        {
+            patient patient = db.patients.Find(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            db.patients.Remove(patient);
+            db.SaveChanges();
+
+            return Ok(patient);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool patientExists(decimal id)
+        {
+            return db.patients.Count(e => e.idPatient == id) > 0;
+        }
+    }
+}
---
+++ WebApiNurseAgenda/WebApiNurseAgenda/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiNurseAgenda.WebApiApplication" Language="C#" %>
